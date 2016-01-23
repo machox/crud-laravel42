6d479ce15e052d8e17b3f4b73dcc81a527e6ed81
machox <bayoe13@gmail.com>
2016-01-23 16:38:31 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,8 +1,9 @@
 /*
 SQLyog Community v11.11 (64 bit)
-MySQL - 5.5.40-0ubuntu0.14.04.1 : Database - techgridtask
+MySQL - 5.5.40-0ubuntu0.14.04.1 : Database - social
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -12,9 +13,9 @@ MySQL - 5.5.40-0ubuntu0.14.04.1 : Database - techgridtask
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`techgridtask` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`social` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-USE `techgridtask`;
+USE `social`;
 
 /*Table structure for table `migrations` */
 
